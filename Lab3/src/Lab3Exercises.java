28d6b027f4ebb8dd63e34ac6c50e78a96e400468
Maria_Cordero <Maria_Cordero@10.31.37.180>
2017-11-15 12:33:39 -0400

    displays box in the center (the box is now squared)

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int screenWidth = screenSize.height;
+		int screenHeight = screenSize.width;
+		
+		JFrame myFrame = new JFrame("Maria");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation((screenHeight/2)-100, (screenWidth/2)-100);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
 	}
 }
